b8770955dda769718a1f7c98ca2222b3c9873086
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-27 15:30:33 +0800

    [ParkingBoy][Ernest] Feat: should not be parked when park multiple cars given multiple car and parking lot only 1 space

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -2,19 +2,27 @@ package com.oocl.cultivation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ParkingLot {
-    private Integer availableSpace;
+    private final Integer capacity;
+    private List<Car> carList;
 
     public ParkingLot() {
-        this.availableSpace = 1;
+        this.capacity = 1;
+        this.carList = new ArrayList<>();
     }
 
-    public ParkingLot(Integer availableSpace) {
-        this.availableSpace = availableSpace;
+    public ParkingLot(Integer capacity) {
+        this.capacity = capacity;
+        this.carList = new ArrayList<>();
     }
 
     public Ticket park(Car car) {
+        if (capacity - carList.size() <= 0) {
+            return null;
+        }
+        carList.add(car);
         return new Ticket();
     }
 }
