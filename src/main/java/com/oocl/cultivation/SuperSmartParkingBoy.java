ee5dcf05ca132c322c08ad22bd7f1edcdb22f909
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-29 01:51:14 +0800

    [SuperSmartParkingBoy][Ernest] Feat: should_park_cars_to_parking_lot_with_higher_available_rate_when_park_given_three_parking_lot

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -9,5 +9,18 @@ public class SuperSmartParkingBoy extends ParkingBoy {
         super(parkingLotList);
     }
 
+    public Double computeAvailableRate(Integer remainingPlace, Integer capacity) {
+        return new Double(remainingPlace) /new Double(capacity);
+    }
 
+    @Override
+    public Ticket park(Car car) throws NotEnoughSpaceException {
+        ParkingLot parkingLotToBeUsed = Collections.max(this.getParkingLotList(), Comparator.comparing(c -> this.computeAvailableRate(c.getRemainingPlace(), c.getCapacity())));
+
+        if (parkingLotToBeUsed.isHasAvailableSlot()) {
+            return parkingLotToBeUsed.park(car);
+        }
+
+        throw new NotEnoughSpaceException("Not Enough Space");
+    }
 }
